8d812f612a85ff87b2d61f3ad3b8b3e7e5504ce7
Xonesix <arjuntx@gmail.com>
2024-10-27 01:47:44 -0500

    Flask-Integration

---
+++ whisper.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+from gpt_text import detectScam_WHISPER
+load_dotenv()
+open_ai_key = os.getenv("OPENAI_API_KEY")
+project_id = os.getenv("PROJECT_ID")
+
+client = OpenAI()
+
+
+
+
+def detectScamCall(filePath):
+
+
+    print(f'Name of File {filePath}')
+    audio_file = open(filePath ,"rb")
+    transcription = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file, 
+    response_format="text"
+    )
+    return transcription
+
