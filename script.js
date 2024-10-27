657cac7f96404ae9659da0c9b2660775516a855e
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2024-10-26 19:43:01 -0600

    213

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-// Toggle Spookify mode
-document.getElementById('spookify-btn').addEventListener('click', () => {
-    document.body.classList.toggle('spooky-mode');
-=======
 document.getElementById('spookify-btn').addEventListener('click', () => {
     document.body.classList.toggle('spooky-mode');
 });
@@ -111,6 +106,56 @@ startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
     setNextQuestion();
->>>>>>> Stashed changes
 });
+trickBtn.addEventListener('click', () => selectAnswer(true));
+treatBtn.addEventListener('click', () => selectAnswer(false));
 
+
+function startGame() {
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+    currentQuestionIndex = 0;
+    score = 0;
+    questionElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    resetState();
+    if (currentQuestionIndex < questions.length) {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+    } else {
+        endGame();
+    }
+}
+
+function showQuestion(question) {
+    questionElement.src = "imgs/" + question;
+}
+
+function resetState() {
+    nextButton.classList.add('hide');
+    trickBtn.classList.remove('hide');
+    treatBtn.classList.remove('hide');
+    questionElement.classList.remove('hide');
+    explanation.classList.add('hide');
+}
+
+function selectAnswer(isScam) {
+    const correct = shuffledQuestions[currentQuestionIndex].isScam === isScam;
+    if (correct) score++;
+    nextButton.classList.remove('hide');
+    questionElement.classList.add('hide');
+    trickBtn.classList.add('hide');
+    treatBtn.classList.add('hide');
+    explanation.classList.remove('hide');
+}
+
+function endGame() {
+    //if score > some amount -> tell them good! otherwise bad! try again
+    questionElement.classList.add('hide');
+    trickBtn.classList.add('hide');
+    treatBtn.classList.add('hide');
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
+}
